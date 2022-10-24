513f985d41c84a2d1b953d763b9316f4c871890f
Maria Eduarda Mendes <maria.m10@aluno.ifsc.edu.br>
2022-10-24 14:12:54 -0300

    Usuario

---
+++ Unplugged-medicines/src/pacote/InterfaceSistema.java
@@ -0,0 +1,24 @@
+package pacote;
+
+public class InterfaceSistema {
+	public void mostarTelaPrincipal(){
+		
+	}
+	
+	public void mostarTelaCadastroRemedio() {
+		
+	}
+	
+	public void mostrarTelaCadastroUsuario() {
+		
+	}
+	
+	public void fazerLigacao() {
+		
+	}
+	
+	public void mostrarTelaLogin() { 
+		
+	}
+
+}
