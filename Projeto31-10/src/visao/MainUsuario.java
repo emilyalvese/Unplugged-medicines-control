5657265e183b176fca1ae7840199f8b49279008e
Andrieli Mendes <84353670+andrieli31@users.noreply.github.com>
2022-11-07 15:12:17 -0300

    Update MainUsuario.java

--- Projeto31-10/src/visao/MainUsuario.java
+++ Projeto31-10/src/visao/MainUsuario.java
@@ -1,10 +1,33 @@
 package visao;
 
+import java.util.Scanner;
+
+import modelo.Usuario;
+
 public class MainUsuario {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Usuario pessoa = new Usuario ();
+		
+		Scanner leitura = new Scanner(System.in);
+		
+		System.out.println("Insira seu nome: ");
+		String nome = leitura.nextLine();
+		if (!nome.isEmpty()) {
+			pessoa.setNome(nome);
+		}
+		System.out.println("Informe seu sexo: ");
+		String sexo = String.valueOf(leitura.nextLine());
+		
+		if (!sexo.isEmpty()) {
+			pessoa.setSexo(sexo);
+		}
+		
+		
+		
+		
+		
 	}
 
 }
